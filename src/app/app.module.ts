9dc6628f1d153d799418d48db980c474df86001c
Natdiv <chrismwamba9@gmail.com>
2019-04-20 00:11:04 +0200

    Ajout du routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostService } from './services/post-service.service';
+import { Routes } from '@angular/router';
+import { NewPostComponent } from './post/new-post/new-post.component';
+
+const appRoutes: Routes = [
+  {path: 'posts', component: PostComponent},
+  {path: 'posts/new', component: NewPostComponent},
+  {path: '', redirectTo: 'posts', pathMatch: 'full'},
+  {path: '', redirectTo: 'post'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
