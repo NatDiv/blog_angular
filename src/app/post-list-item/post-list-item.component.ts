c61fbe0f08d0e849b7d61fe41d29859c68c3caf4
Natdiv <chrismwamba9@gmail.com>
2019-05-02 21:08:40 +0200

    Creation du component qui liste tous les posts

---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {PostService} from '../services/post-service.service';
+import {Post} from '../models/post.model';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent implements OnInit {
+
+  posts: Post[] = [];
+  postsSubscription: Subscription;
+  constructor(private postService: PostService) { }
+
+  ngOnInit() {
+    this.postsSubscription = this.postService.postSubject.subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+    this.postService.emitPost();
+  }
+
+}
